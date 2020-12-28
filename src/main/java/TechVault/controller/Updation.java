4f0d6a5ccd261e33ea0218fb635ddb8bcef02696
ManishKumarKeshri <manish.keshri562@gmail.com>
2020-12-28 13:33:57 -0800

    Removing unnecessry things.
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- src/main/java/TechVault/controller/Updation.java
+++ src/main/java/TechVault/controller/Updation.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
-@RequestMapping("home")
+@RequestMapping("update")
 public class Updation {
     /**
      * To post a single like by a user.
