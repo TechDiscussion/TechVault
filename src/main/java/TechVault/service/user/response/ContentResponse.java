632621d445aa0a3743629ea84a61006d1ee3ef20
ManishKumarKeshri <manish.keshri562@gmail.com>
2021-01-03 00:21:01 -0800

    Updating.
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- src/main/java/TechVault/service/user/response/ContentResponse.java
+++ src/main/java/TechVault/service/user/response/ContentResponse.java
@@ -2,17 +2,17 @@ package TechVault.service.user.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ContentResponse {
     private String title;
     private String source;
     private String timestamp;
+
+    @JsonIgnore
     private HttpStatus status;
 
     public ContentResponse(String title, String company, String datePosted, HttpStatus httpStatus) {
