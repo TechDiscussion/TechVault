cb5b6e5538c91d3e6306bd7de9fc675d8bf21d18
Prateek Shroff <prateekshroff.04@gmail.com>
2021-02-13 11:06:15 -0800

    Get blogs vount by Company Name

--- src/main/java/TechVault/services/homepage/controller/HomepageController.java
+++ src/main/java/TechVault/services/homepage/controller/HomepageController.java
@@ -2,6 +2,8 @@ package TechVault.services.homepage.controller;
 
 import TechVault.services.homepage.HomepageService;
 import TechVault.services.homepage.model.Blog;
+import TechVault.services.homepage.model.CompanyCount;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,5 +86,21 @@ public class HomepageController {
             return new ResponseEntity<>("ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(blogs, HttpStatus.OK);
+    }
+
+    /**
+     * To get count of blogs by company.
+     * @return A Response entity which will have all the blogs to be loaded as home page.
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/blogsCount")
+    public ResponseEntity<?> getBlogsCount() {
+        List<CompanyCount> blogsCount = null;
+        try {
+            blogsCount = service.getBlogsCountByCompany();
+        } catch (Exception e) {
+            LOGGER.error("Unable to get blogs Count ", e);
+            return new ResponseEntity<>("ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(blogsCount, HttpStatus.OK);
     }
 }
--- src/main/java/TechVault/services/homepage/persistence/HomepageDaoImpl.java
+++ src/main/java/TechVault/services/homepage/persistence/HomepageDaoImpl.java
@@ -1,6 +1,8 @@
 package TechVault.services.homepage.persistence;
 
 import TechVault.services.homepage.model.Blog;
+import TechVault.services.homepage.model.CompanyCount;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,10 +20,16 @@ public class HomepageDaoImpl implements HomepageDao {
 
     @Override
     public List<Blog> getBlog(Integer pageNo, Integer pageSize, String sortBy) {
-        Pageable pageable = PageRequest.of(pageNo, pageSize,
-                Sort.by(sortBy).descending());
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
         Page<Blog> page = repo.findAll(pageable);
 
         return page.getContent();
+    }
+
+    @Override
+    public List<CompanyCount> getBlogsCountBy(String companyOrConference) {
+        List<CompanyCount> blogGroups = repo.groupBlogsBy(companyOrConference);
+        blogGroups.stream().filter((blogGroup) -> blogGroup.getCompany() != null);
+        return blogGroups;
     }
 }
