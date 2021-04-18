dedc822856a9af613d45e353e9706cdcb8537ef4
ManishKumarKeshri <manish.keshri562@gmail.com>
2021-04-17 20:01:16 -0700

    Removing required.
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- src/main/java/TechVault/services/homepage/request/SearchByCompanyOrKeywordRequest.java
+++ src/main/java/TechVault/services/homepage/request/SearchByCompanyOrKeywordRequest.java
@@ -2,11 +2,9 @@ package TechVault.services.homepage.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Data
 public class SearchByCompanyOrKeywordRequest {
-    @NotBlank
     private List<String> searchWords;
 }
